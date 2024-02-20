ea35476067d89cd740ebf8733bad7dbf0ccb7a36
JoshLo22 <joshua.lo25@ycdsbk12.ca>
2024-02-20 11:35:31 -0500

    modified:   AreaSin.java 	modified:   FtoC.java

--- AreaSin.java
+++ AreaSin.java
@@ -1,13 +1,27 @@
 class AreaSin extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program AreaSin.java that uses the Math.sin() function to calculate the area of a triangle, given two sides and the contained angle
+  * @author: Josh Lo
   */
   
   public void run() {
     
-    // start coding here
+    // Declare variables
+    double dblSide1;
+    double dblSide2;
+    double dblAngle1;
+    double dblArea;
+
+    // Get variables
+    dblSide1 = readDouble("Please insert the first side length: ");
+    dblSide2 = readDouble("Please insert the second side length: ");
+    dblAngle1 = readDouble("Please insert the first angle in radians: ");
+  
+    // Calculate
+    dblArea = dblSide1 * dblSide2 * Math.sin(dblAngle1) / 2;
     
+    // Show result
+    System.out.println("The area of your shape is " + dblArea);
   }
 }
--- FtoC.java
+++ FtoC.java
@@ -1,13 +1,27 @@
 class FtoC extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program FtoC.java that lets you enter a degree measure in Fahrenheit, and that prints the result in celsius degree measure
+  * @author:Josh Lo
   */
   
   public void run() {
     
-    // start coding here
-    
+    // Declare variables
+    int intTempFar;
+    double dblTempFar;
+    double dblTempCel;
+    int intTempCel;
+
+    // Get variables
+    intTempFar = readInt("Insert the tempature in Farenhiet: ");
+
+    // Calculate
+    dblTempFar = (double)intTempFar;
+    dblTempCel = (dblTempFar - 32) * 5 / 9;
+    intTempCel = (int)dblTempCel;
+
+    // Show result
+    System.out.println("The tempature is " + intTempCel + "°C");
   }
 }
